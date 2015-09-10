726e316e507e96b0c0bd944680006e1512d0380b
AniahLiepard <aniah.garcia@techhigh.us>
2015-09-10 10:25:11 -0400

    Use Methods With Parameters Project
    https://www.thenewboston.com/videos.php?cat=31&amp;video=17980

---
+++ ProjectTwelve.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ProjectTwelve {
+	public static void main(String[] args){
+		Scanner identity = new Scanner(System.in);
+		String fname = input.nextLine();
+		inputObject.fname(fname);
+		ProjectTwelvePart2 inputObject = new ProjectTwelvePart2();
+			System.out.println("What is your friend's name?");
+		
+	}
+}
---
+++ ProjectTwelvePart2.java
@@ -0,0 +1,5 @@
+public class ProjectTwelvePart2 {
+	public void message(String fname[]){
+		System.out.println("How is it going with " + fname);
+	}
+}
